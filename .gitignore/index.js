b3d70f7948dac71ddceaf1910993c59d3153315d
ytzmo <64926958+ytzmo@users.noreply.github.com>
2020-09-13 12:47:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,32 +4,16 @@ const client = new Discord.Client();
 
 
 
-
-
 var config_token = ("TOKEN") //token du bot  
 var config_activity = ("ACTIVITY") //activité du bot
 var channel_id = ("CHANNEL_ID")	//id du salon ou il y aura le claim faisable
 
 
-
-
-
-
-
-
-
-
-
-
 bot.on("ready", async () => {
   console.log("\n" + bot.user.tag + ` est pret ! \n\nCreator : ytzmo#8888`)
   bot.user.setActivity(`${config_activity}`, { url: "https://www.twitch.tv/nitro", type: "STREAMING" })
 
 });
-
-
-
-
 
 
 bot.on("message", message => {
@@ -56,6 +40,7 @@ bot.on("message", message => {
     }
 
 
+  
 
   if (message.content.startsWith("!claim")) {
 
@@ -73,96 +58,8 @@ bot.on("message", message => {
           message.author.send("http://discord.gift/" + rasteleSembol(16, '0Aa'))
 
 
-
-
-
-
-
-
-
-
-
-
-
-bot.on("ready", async () => {
-  console.log("\n" + bot.user.tag + ` est pret ! \n\nCreator : ytzmo#8888`)
-  bot.user.setActivity(`${config_activity}`, { url: "https://www.twitch.tv/nitro", type: "STREAMING" })
-
 });
-
-
-
-
-
-
-bot.on("message", message => {
-
-  user = message.mentions.users.first();
-  if (message.author.bot) return;
-  let args = message.content.split(" ").slice(1);
-
-
-  function rasteleSembol(uzunluk, semboller) {
-
-    var maske = '';
-    if (semboller.indexOf('a') > -1) maske += 'abcdefghijklmnopqrstuvwxyz';
-    if (semboller.indexOf('A') > -1) maske += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (semboller.indexOf('0') > -1) maske += '0123456789'; 
-    var sonuc = '';
-     
-    for (var i = uzunluk; i > 0; --i)
-    {
-    sonuc += maske[Math.floor(Math.random() * maske.length)];
-    }
-    return sonuc;
-     
-    }
-
-
-
-  if (message.content.startsWith("!claim")) {
-
-    if (message.channel.type === "dm") return;
-    else {
-
-      if (message.channel.id === channel_id) {
-
-        {
-          var embed_embed = new Discord.MessageEmbed()
-            .setDescription(`**nitro envoyé à <@${message.author.id}> **`)
-            .setColor("#2f3136")
-
-          message.channel.send(embed_embed)
-          message.author.send("http://discord.gift/" + rasteleSembol(16, '0Aa'))
-
-
-
-        }
-      }
-
-
-    }
-  }
-
-
-
-
-
-
-});
-
-
-bot.login(config_token)
-        }
-      }
-
-
-    }
-  }
-
-
-
-
+      
 
 
 });
